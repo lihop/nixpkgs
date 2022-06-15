5b233e4c1e3a8f5e8eef5a0ec29b3f1b65a593e3
rotaerk <m.scott.stewart@gmail.com>
2022-06-15 18:17:45 -0400

    maintainers: add rotaerk

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -13588,6 +13588,12 @@
     github = "RossComputerGuy";
     githubId = 19699320;
   };
+  rotaerk = {
+    name = "Matthew Stewart";
+    email = "m.scott.stewart@gmail.com";
+    github = "rotaerk";
+    githubId = 17690823;
+  };
   rowanG077 = {
     email = "goemansrowan@gmail.com";
     github = "rowanG077";
