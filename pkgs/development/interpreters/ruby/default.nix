2698c07be777afc5471d65cfb03ce9266e869152
Leroy Hopson <git@leroy.geek.nz>
2023-06-01 09:55:08 +1200

    Revert "ruby_3_2: 3.2.1 -> 3.2.2"
    This reverts commit 3f2cadf4ea0c6233dd945fde0dddeb2cd17a4c2f.

--- pkgs/development/interpreters/ruby/default.nix
+++ pkgs/development/interpreters/ruby/default.nix
@@ -345,8 +345,8 @@ in {
   };
 
   ruby_3_2 = generic {
-    version = rubyVersion "3" "2" "2" "";
-    sha256 = "sha256-lsV1WIcaZ0jeW8nydOk/S1qtBs2PN776Do2U57ikI7w=";
+    version = rubyVersion "3" "2" "1" "";
+    sha256 = "sha256-E9Z5AWYO4yF9vZ3VYFk0a9QhLOZKacMG71LfZJNfjb0=";
     cargoSha256 = "sha256-6du7RJo0DH+eYMOoh3L31F3aqfR5+iG1iKauSV1uNcQ=";
   };
 }
